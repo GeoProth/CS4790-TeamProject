3867b2e0431fab9a4299d112c6f4e48bfcd44392
travisredd <travisredd@mail.weber.edu>
2019-07-19 21:24:08 -0600

    Set up datatable.net plugin for vendor view

--- CS4790TeamProject/wwwroot/js/site.js
+++ CS4790TeamProject/wwwroot/js/site.js
@@ -7,28 +7,35 @@
 //Initialising DataTables
 $(document).ready(function () {
     var table1 = $('#table_inventory').DataTable({
+
+        columnDefs: [
+            { width:198, targets: 10}
+        ],
         responsive: true
     });
 
     var table2 = $('#table_inventory2').DataTable({
+        columnDefs: [
+            { width: 20, targets: 4}
+        ],
         responsive: true
     });
 
-    var table3 = $('#table_orders').DataTable({
+    var table3 = $('#table_vendor').DataTable({
         responsive: true
     });
 
-    var table4 = $('#table_orders2').DataTable({
-        responsive: true
-    });
+    //var table4 = $('#table_orders2').DataTable({
+    //    responsive: true
+    //});
 
-    var table5 = $('#table_orders3').DataTable({
-        responsive: true
-    });
+    //var table5 = $('#table_orders3').DataTable({
+    //    responsive: true
+    //});
 
-    var table6 = $('#table_orders4').DataTable({
-        responsive: true
-    });
+    //var table6 = $('#table_orders4').DataTable({
+    //    responsive: true
+    //});
 
     table1.column(1).visible(false);
     table1.column(3).visible(false);
@@ -37,13 +44,18 @@ $(document).ready(function () {
     table1.column(7).visible(false);
     table1.column(8).visible(false);
     table1.column(9).visible(false);
-    //table1.column(6).visible(false);
+
+    //table2.column(4).visible(false);
+
+    $('#today').val(new Date().toDateInputValue());
+    $('#today2').val(new Date().toDateInputValue());
 
 
-    let today = new Date().toISOString().substr(0, 10);
-    document.querySelector("#today").value = today;
-    document.querySelector("#today2").value = today;
 
 });
 
-
+Date.prototype.toDateInputValue = (function () {
+    var local = new Date(this);
+    local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+    return local.toJSON().slice(0, 10);
+});

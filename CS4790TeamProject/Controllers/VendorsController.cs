5dfe91e6f3a603c6c94dd863402824ec6d835921
Brandon Muhlestein <brandonmuhlestein@mail.weber.edu>
2019-07-24 23:29:22 -0600

    Updates to VendorController and VendorHistoryViewModel.

--- CS4790TeamProject/Controllers/VendorsController.cs
+++ CS4790TeamProject/Controllers/VendorsController.cs
@@ -15,9 +15,18 @@ namespace CS4790TeamProject.Views.Vendors
     {
         private readonly ApplicationDbContext _context;
 
+        public VendorHistoryViewModel VendorHistoryVM { get; set; }
+
         public VendorsController(ApplicationDbContext context)
         {
             _context = context;
+
+            VendorHistoryVM = new VendorHistoryViewModel()
+            {
+                Vendors = new Models.Vendor(),
+                PurchaseOrders = _context.PurchaseOrder
+                    .ToList()
+            };
         }
 
         // GET: Vendors
@@ -46,31 +55,57 @@ namespace CS4790TeamProject.Views.Vendors
 
         public async Task<IActionResult> History(int id)
         {
+            VendorHistoryVM.Vendors = await _context.Vendor
+                .Include(m => m.PurchaseOrders)
+                .SingleOrDefaultAsync(n => n.VendorId == id);
+
+            if (VendorHistoryVM.Vendors == null)
+            {
+                return NotFound();
+            }
+
+            return View(VendorHistoryVM);
+        }
+
+        /*
+        public async Task History(int? id)
+        {
+            var Vendor = new VendorHistoryViewModel();
+            Vendor.Vendors = await _context.Vendor
+                .Include(v => v.PurchaseOrders)
+                .AsNoTracking()
+                .OrderBy(i => v.PurchaseDate)
+                .ToListAsync();
+            /*
             var vendor = await _context.Vendor
                 .FirstOrDefaultAsync(m => m.VendorId == id);
 
             var orders = new List<PurchaseOrder>();
+            */
+        //var orders = new List<PurchaseOrder>(await _context.PurchaseOrder.Where(n => n.VendorID == id));
 
-            //var orders = new List<PurchaseOrder>(await _context.PurchaseOrder.Where(n => n.VendorID == id));
+        /****
+        var order = await _context.PurchaseOrder
+            .FirstOrDefaultAsync(n => n.VendorID == id);
+            ****/
 
-            /****
-            var order = await _context.PurchaseOrder
-                .FirstOrDefaultAsync(n => n.VendorID == id);
-                ****/
+        /***
+        var orders = await _context.PurchaseOrder
+            .Where(VendorId == id);
+            ***/
 
-           /***
-           var orders = await _context.PurchaseOrder
-               .Where(VendorId == id);
-               ***/
+        /*
+        var VendorHistoryViewModel = new VendorHistoryViewModel
+        {
+            Vendors = vendor,
+            PurchaseOrders = orders
+        };
+        */
 
-            var VendorHistoryViewModel = new VendorHistoryViewModel
-            {
-                Vendors = vendor,
-                PurchaseOrders = orders
-            };
-
-            return View(VendorHistoryViewModel);
-        }
+        //return View(Vendor);
+        /*
+    }
+    */
 
         // GET: Vendors/Create
         public IActionResult Create()

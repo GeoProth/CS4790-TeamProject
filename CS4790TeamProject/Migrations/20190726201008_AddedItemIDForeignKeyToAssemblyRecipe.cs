ed280004de4abb486b28c579490155a52792ea49
Taylor Dawson <thecanadian0504@gmail.com>
2019-07-28 17:00:59 -0600

    Database additions

---
+++ CS4790TeamProject/Migrations/20190726201008_AddedItemIDForeignKeyToAssemblyRecipe.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS4790TeamProject.Migrations
+{
+    public partial class AddedItemIDForeignKeyToAssemblyRecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

0cf264b684e3777a158df500ec1c03a70ad85dce
Geo <Gprothero@live.com>
2019-08-03 13:56:10 -0600

    Create Orders Update

---
+++ CS4790TeamProject/Migrations/20190801192104_AddedDeliveryDateToPurchaseOrders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS4790TeamProject.Migrations
+{
+    public partial class AddedDeliveryDateToPurchaseOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveryDate",
+                table: "PurchaseOrder",
+                nullable: true);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryDate",
+                table: "PurchaseOrder");
+
+        }
+    }
+}

9bdd97a4b263724a7766df2f1f29b1f78038ada1
Geo <Gprothero@live.com>
2019-08-13 16:14:25 -0600

    merged Geo

---
+++ CS4790TeamProject/Models/ViewModels/RecieveViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS4790TeamProject.Models.ViewModels
+{
+    public class RecieveViewModel
+    {
+        public PurchaseOrder PurchaseOrder { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+
+        public List<RecievedItems> RecievedItems { get; set; }
+    }
+}

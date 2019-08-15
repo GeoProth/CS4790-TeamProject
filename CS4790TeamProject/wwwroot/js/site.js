6ae993fc14e0afb4bbef3371f701385959f957a8
travisredd <travisredd@mail.weber.edu>
2019-08-14 18:23:15 -0600

    Added datatables.net plug in for vendors history table. Also disabled links to pages that don't work.

--- CS4790TeamProject/wwwroot/js/site.js
+++ CS4790TeamProject/wwwroot/js/site.js
@@ -1,22 +1,21 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿
+
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
 
 //Initialising DataTables
 $(document).ready(function () {
     var table1 = $('#table_inventory').DataTable({
 
         columnDefs: [
-            { width:198, targets: 10}
+            { width: 198, targets: 10 }
         ],
         responsive: true
     });
 
     var table2 = $('#table_inventory2').DataTable({
         columnDefs: [
-            { width: 20, targets: 4}
+            { width: 20, targets: 4 }
         ],
         responsive: true
     });
@@ -40,10 +39,11 @@ $(document).ready(function () {
 
     var table5 = $('#table_POdetails').DataTable({
         responsive: true
+    });
 
-
-
-    })
+    var table6 = $('#table_PHvendor').DataTable({
+        responsive: true
+    });
 
     $("#myInputAll").on("keyup", function () {
         var value = $(this).val().toLowerCase();
@@ -73,7 +73,6 @@ $(document).ready(function () {
         });
     });
 
-  
     $("#Add").on("click", function () {
         $("#selectboxDIV").append("<label asp-for='itemID' class='control-label'></label><select name='itemID' id='listbox' class='form-control'> @foreach (var item in Model.allItems){<option value='@item.ItemId'>@item.ItemName</option>}</select>");
     });
@@ -81,35 +80,16 @@ $(document).ready(function () {
     $("#Remove").on("click", function () {
         $("#selectboxDIV").children().last().remove();
     });
-   
-
-    //var table4 = $('#table_orders2').DataTable({
-    //    responsive: true
-    //});
-
-    //var table5 = $('#table_orders3').DataTable({
-    //    responsive: true
-    //});
-
-    //var table6 = $('#table_orders4').DataTable({
-    //    responsive: true
-    //});
 
     table1.column(1).visible(false);
     table1.column(3).visible(false);
-    //table1.column(5).visible(false);
     table1.column(6).visible(false);
     table1.column(7).visible(false);
     table1.column(8).visible(false);
     table1.column(9).visible(false);
 
-
-    //table2.column(4).visible(false);
-
     $('#today').val(new Date().toDateInputValue());
     $('#today2').val(new Date().toDateInputValue());
-
-
 
 });
 
